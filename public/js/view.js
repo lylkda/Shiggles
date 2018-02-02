5cf8b1264ac010c08049e33ff49fb962c2af1b74
Marissa Mekvichitsaeng <marissa.mekvichitsaeng@gmail.com>
2018-02-01 20:45:47 -0800

    added js folder to public folder

---
+++ public/js/view.js
@@ -0,0 +1,7 @@
+$(document).ready(()=>{
+
+	let $newQuestionInput = $('input.new-q');
+	let $newAnswer1 = $('newQanswer1');
+	let $newAnswer2 = $('newQanswer2');
+	
+})
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,9 @@ let db = require("../models");
 module.exports = (app)=> {
 	//GET the current question
 	app.get("/api/questions", (req, res) => {
-		db.NewQuestion.findAll({}).then((dbNewQuestion) => {
+		db.NewQuestion.findAll({
+			
+		}).then((dbNewQuestion) => {
 			console.log(res.json(dbNewQuestion));
 		});
 	});
