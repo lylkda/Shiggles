1007453887335f9481a919f7762dc05974eb9e32
Marissa Mekvichitsaeng <marissa.mekvichitsaeng@gmail.com>
2018-02-01 19:42:51 -0800

    built out questions.js in module

---
+++ models/questions.js
@@ -0,0 +1,41 @@
+module.exports = function(sequelize, DataTypes) {
+	let NewQuestion = sequelize.define("NewQuestion", {
+		question: {
+			type: DataTypes.STRING,
+			allowNull: false, 
+			validate: {
+				len: [1, 200]
+			}
+		},
+		answer1: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 75]
+			}
+		},
+		answer2: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 75]
+			}
+		},
+		isCurrent: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: false
+		},
+		a1Votes: {
+			type: DataTypes.INTEGER
+		},
+		a2Votes: {
+			type: DataTypes.INTEGER
+		},
+		isComplete: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: false
+		}
+	});
+
+
+};
