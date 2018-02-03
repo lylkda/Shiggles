2adb3f43bf5f6026639306c8e12f6768292c8806
Marissa Mekvichitsaeng <marissa.mekvichitsaeng@gmail.com>
2018-02-03 12:05:49 -0800

    post route added, view.js borked

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,9 +4,22 @@ module.exports = (app)=> {
 	//GET the current question
 	app.get("/api/questions", (req, res) => {
 		db.NewQuestion.findAll({
-			
+			where: {
+				isCurrent: true
+			}
 		}).then((dbNewQuestion) => {
-			console.log(res.json(dbNewQuestion));
+			res.json(dbNewQuestion);
+		});
+	});
+
+	//POST new questions
+	app.post("/api/questions", (req, res) => {
+		db.NewQuestion.create({
+			question: req.body.question,
+			answer1: req.body.answer1,
+			answer2: req.body.answer2
+		}).then((dbNewQuestion) => {
+			res.json(dbNewQuestion);
 		});
 	});
 };
