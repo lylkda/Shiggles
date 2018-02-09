c443d3a884e8474e36856c36327c8b38648ca61b
Linda Ly <31869482+lylkda@users.noreply.github.com>
2018-02-08 21:19:00 -0800

    Update connection.js
    pw null

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "dor3mon1",
+    password: null,
     database: "heroku_29543ed04cd0575"
 });
 
